c69ea5735488efcef128bc9468552b81dd66aa77
Fabian Toapanta <fabianRBN_95@hotmail.com>
2020-02-21 10:54:21 -0500

    first commit

--- assets/js/webview.js
+++ assets/js/webview.js
@@ -1,3 +1,5 @@
+var os = require('os')
+
 window.onresize = doLayout;
 
 onload = () => {
@@ -7,8 +9,22 @@ onload = () => {
   printButton();
 };
 
+var rand = function() {
+  return Math.random().toString(36).substr(2); // remove `0.`
+};
+
+var token = function() {
+  return rand() + rand(); // to make it longer
+};
+
+
+
 function doLayout() {
   let webview = document.querySelector("webview");
+  let hostname = os.hostname;
+  webview.setAttribute('src','http://192.168.10.177:3000/?token='+token()+'&hostname='+hostname); 
+/*   webview.setAttribute('src','http://192.168.10.177:8080'); */
+
   let windowWidth = document.documentElement.clientWidth;
   let windowHeight = document.documentElement.clientHeight;
   let controlsHeight = getControlsHeight();
